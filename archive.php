8311149811366ab45c8eba8f6546772cb011bc92
rodica-andronache <rodik_andronache@yahoo.com>
2015-11-12 15:34:16 +0200

    !!! Fixed top header for archive page

--- archive.php
+++ archive.php
@@ -15,19 +15,37 @@
 </header>
 <!-- /END HOME / HEADER  -->
 
-<div class="archive-top">
-	<div class="section-overlay-layer">
-		<div class="container">
+<?php
+	$parallax_one_blog_header_image = get_theme_mod( 'parallax_one_blog_header_image', parallax_get_file('/images/background-images/background-blog.jpg') );
+	$parallax_one_blog_header_title = get_theme_mod( 'parallax_one_blog_header_title', esc_html__('BLOG','llorix-one')  );
+	$parallax_one_blog_header_subtitle = get_theme_mod( 'parallax_one_blog_header_subtitle', esc_html__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis risus augue. Cras at mollis eros. Sed porttitor quam eget aliquam mattis. Fusce leo nibh, ornare at volutpat ut, luctus dictum elit. Mauris non vehicula eros, ac lacinia lorem. Quisque fermentum purus ac scelerisque suscipit. Morbi et iaculis tellus. Proin ut urna ac purus suscipit iaculis. Aliquam erat volutpat. Donec at viverra magna. Fusce efficitur eros a nunc volutpat ultrices. Aenean mattis purus lectus, quis fermentum diam placerat in.','llorix-one') );
+	
+	if( !empty($parallax_one_blog_header_image) || !empty($parallax_one_blog_header_title) || !empty($parallax_one_blog_header_subtitle) ):
+		
+		if( !empty($parallax_one_blog_header_image) ):
+			echo '<div class="archive-top" style="background-image: url('.$parallax_one_blog_header_image.');">';
+		else:
+			echo '<div class="archive-top">';
+		endif;
+				echo '<div class="section-overlay-layer">';
+					echo '<div class="container">';
 
-			<p class="archive-top-big-title">Lorem ipsum dolor</p>
-			<p class="colored-line"></p>
-			<p class="archive-top-text">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis risus augue. Cras at mollis eros. Sed porttitor quam eget aliquam mattis. Fusce leo nibh, ornare at volutpat ut, luctus dictum elit. Mauris non vehicula eros, ac lacinia lorem. Quisque fermentum purus ac scelerisque suscipit. Morbi et iaculis tellus. Proin ut urna ac purus suscipit iaculis. Aliquam erat volutpat. Donec at viverra magna. Fusce efficitur eros a nunc volutpat ultrices. Aenean mattis purus lectus, quis fermentum diam placerat in.
-			</p>
+						if( !empty($parallax_one_blog_header_title) ):
+							echo '<p class="archive-top-big-title">'.$parallax_one_blog_header_title.'</p>';
+							echo '<p class="colored-line"></p>';							
+						endif;
+						
+						if( !empty($parallax_one_blog_header_subtitle) ):
+							echo '<p class="archive-top-text">'.$parallax_one_blog_header_subtitle.'</p>';
+						endif;	
 
-		</div>
-	</div>
-</div>
+					echo '</div>';
+				echo '</div>';
+			echo '</div>';
+		
+	endif;
+	
+?>
 
 
 <div role="main" id="content" class="content-warp">
